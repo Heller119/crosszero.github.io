4ef8e8bcea6675ccfa256c5ddaf6b01418d0c144
Heller119 <duksanemail@gmail.com>
2020-03-13 21:42:11 +0300

    first commit

---
+++ scripts/myscript.js
@@ -0,0 +1,137 @@
+var player1 = "O";
+var player2 = "X";
+var motion = 1;
+function Click() {
+		document.getElementById("butt");
+		if (motion == 1) {
+		butt.value = player1;
+		motion = 2;
+		butt.disabled = 1;
+		}
+		else if (motion == 2) {
+		butt.value = player2;
+		motion = 1;
+		butt.disabled = 1;
+		}
+		
+}
+
+function Click2() {
+		document.getElementById("butt2");
+		if (motion == 1) {
+		butt2.value = player1;
+		motion = 2;
+		butt2.disabled = 1;
+		}
+		else if (motion == 2) {
+		butt2.value = player2;
+		motion = 1;
+		butt2.disabled = 1;
+		}
+		
+}
+
+function Click3() {
+		document.getElementById("butt3");
+		if (motion == 1) {
+		butt3.value = player1;
+		motion = 2;
+		butt3.disabled = 1;
+		}
+		else if (motion == 2) {
+		butt3.value = player2;
+		motion = 1;
+		butt3.disabled = 1;
+		}
+		
+}
+
+function Click4() {
+		document.getElementById("butt4");
+		if (motion == 1) {
+		butt4.value = player1;
+		motion = 2;
+		butt4.disabled = 1;
+		}
+		else if (motion == 2) {
+		butt4.value = player2;
+		motion = 1;
+		butt4.disabled = 1;
+		}
+		
+}
+
+function Click5() {
+		document.getElementById("butt5");
+		if (motion == 1) {
+		butt5.value = player1;
+		motion = 2;
+		butt5.disabled = 1;
+		}
+		else if (motion == 2) {
+		butt5.value = player2;
+		motion = 1;
+		butt5.disabled = 1;
+		}
+		
+}
+
+function Click6() {
+		document.getElementById("butt6");
+		if (motion == 1) {
+		butt6.value = player1;
+		motion = 2;
+		butt6.disabled = 1;
+		}
+		else if (motion == 2) {
+		butt6.value = player2;
+		motion = 1;
+		butt6.disabled = 1;
+		}
+		
+}
+
+function Click7() {
+		document.getElementById("butt7");
+		if (motion == 1) {
+		butt7.value = player1;
+		motion = 2;
+		butt7.disabled = 1;
+		}
+		else if (motion == 2) {
+		butt7.value = player2;
+		motion = 1;
+		butt7.disabled = 1;
+		}
+		
+}
+
+function Click8() {
+		document.getElementById("butt8");
+		if (motion == 1) {
+		butt8.value = player1;
+		motion = 2;
+		butt8.disabled = 1;
+		}
+		else if (motion == 2) {
+		butt8.value = player2;
+		motion = 1;
+		butt8.disabled = 1;
+		}
+		
+}
+
+function Click9() {
+		document.getElementById("butt9");
+		if (motion == 1) {
+		butt9.value = player1;
+		motion = 2;
+		butt9.disabled = 1;
+		}
+		else if (motion == 2) {
+		butt9.value = player2;
+		motion = 1;
+		butt9.disabled = 1;
+		}
+		
+}
